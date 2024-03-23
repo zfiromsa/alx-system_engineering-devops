6b4f415c46ebd2397df3d3b99cfa70dc4ff9cf14
zfiromsa <mdzm841@gmail.com>
2024-03-22 18:11:25 -0700

    go

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -4,10 +4,10 @@
 # specific permissions, owner, and group.
 
 file { '/tmp/school':
-    ensure => file,
-    mode => '0744',
-    owner => 'www-data',
-    group => 'www-data',
-    content => "I love Puppet"
+ensure => file,
+mode => '0744',
+owner => 'www-data',
+group => 'www-data',
+content => "I love Puppet"
 }
 
