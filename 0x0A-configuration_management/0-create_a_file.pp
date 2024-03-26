fb51516759789235a2e93f628f7d36eabd0f8d51
zfiromsa <mdzm841@gmail.com>
2024-03-26 05:39:00 -0700

    go

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,10 +1,10 @@
 # Puppet manifest to create a file in /tmp with 
 # specific permissions, owner, and group.
 file { '/tmp/school':
-ensure => file,
-mode => '0744',
-owner => 'www-data',
-group => 'www-data',
+ensure  => file,
+mode    => '0744',
+owner   => 'www-data',
+group   => 'www-data',
 content => "I love Puppet"
 }
 
