408cee987d230dda85b73dca3c60f407276ba1b7
zfiromsa <111040876+zfiromsa@users.noreply.github.com>
2024-05-21 10:27:26 +0300

    Delete 0x1B-web_stack_debugging_4 directory

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++
@@ -1,7 +0,0 @@
-# Increas the ULIMIT for Nginx
-
-exec { 'fix--for-nginx':
-	command => "sudo sed -i 's/15/4096/' /etc/default/nginx; sudo service nginx restart",
-	path    => ['/bin', '/usr/bin', '/usr/sbin']
-}
-
